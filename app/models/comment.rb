4a62b1389b4a7455e86fcdba6cef1d0622185387
88JumpingJacks <li.jack2006@hotmail.com>
2014-11-30 00:17:16 -0500

    Added comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ActiveRecord::Base
-  belongs_to :article
+  belongs_to :article 
+  validates :commenter, presence: true, length: { minimum: 1}
+  validates :body, presence: true, length: { minimum: 1}
+  # Note: implement feature to show user errors if they
+  # cannot create comment!!!
 end
